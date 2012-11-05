25865f7c7cfa5d8c6d1ea7c4de4566d334363c28
Louis Roché <louis@louisroche.net>
2012-11-05 23:42:26 +0100

    debut du protocole avec les protobuffers et un bout de code Go qui les utilisent

---
+++ go_exemple_proto/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"code.google.com/p/goprotobuf/proto"
+	"log"
+	"fmt"
+	"obi-wan-kanbanobi/protocole"
+)
+
+func main() {
+	cible := message.CIBLE_USERS
+	cmd := message.CMD_Y
+	test := &message.Msg{
+	Cible: &cible,
+	Users: &message.Msg_Users{
+		Cmd: &cmd,
+		},
+	}
+	// test := &example.Test{
+	// 	Label: proto.String("hello world"),
+	// 	Type:  proto.Int32(17),
+	// 	Optionalgroup: &example.Test_OptionalGroup{
+	// 		RequiredField: proto.String("good bye"),
+	// 	},
+	// }
+	data, err := proto.Marshal(test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newTest := &message.Msg{}
+	fmt.Println(data, len(data), string(data))
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if test.GetProjects() != newTest.GetProjects() {
+		log.Fatalf("data mismatch %q != %q", test.GetProjects(), newTest.GetProjects())
+	}
+}
