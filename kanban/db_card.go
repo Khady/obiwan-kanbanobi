820cbaa9fbd2b107dcf47c436a4f6de995f52219
Louis Roché <louis@louisroche.net>
2012-11-12 19:32:19 +0100

    utilisation de la dbpool dans db_card

--- kanban/db_card.go
+++ kanban/db_card.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"database/sql"
-	_ "github.com/bmizerany/pq"
-)
-
 type Card struct {
 	Id         int
 	Name       string
@@ -17,24 +12,32 @@ type Card struct {
 	Write      string
 }
 
-func (c *Card) Update(db *sql.DB) error {
+func (c *Card) Update(p *ConnectionPoolWrapper) error {
+	db := p.GetConnection()
+	defer p.ReleaseConnection(db)
 	_, err := db.Exec(`update cards set name = $1, content = $2, column_id = $3, project_id = $4,
 tags = $5, users_id = $6, scripts_id = $7, write = $8 where id = $9`,
 		c.Name, c.Content, c.Column_id, c.Project_id, c.Tags, c.Users_id, c.Scripts_id, c.Write, c.Id)
 	return err
 }
 
-func (c *Card) ChangeName(db *sql.DB) error {
+func (c *Card) ChangeName(p *ConnectionPoolWrapper) error {
+	db := p.GetConnection()
+	defer p.ReleaseConnection(db)
 	_, err := db.Exec("update cards set name = $1 where id = $2", c.Name, c.Id)
 	return err
 }
 
-func (c *Card) ChangeContent(db *sql.DB) error {
+func (c *Card) ChangeContent(p *ConnectionPoolWrapper) error {
+	db := p.GetConnection()
+	defer p.ReleaseConnection(db)
 	_, err := db.Exec("update cards set content = $1 where id = $2", c.Content, c.Id)
 	return err
 }
 
-func (c *Card) UpdateWrite(db *sql.DB, write string) error {
+func (c *Card) UpdateWrite(p *ConnectionPoolWrapper, write string) error {
+	db := p.GetConnection()
+	defer p.ReleaseConnection(db)
 	db = db
 	c.Id = c.Id
 	write = write
@@ -56,7 +59,9 @@ func (c *Card) addIdInCell(cell string) (string, error) {
 // Ajoute des droits de modifications sur la carte a une personne.
 // Modifie la chaine deja existante pour y ajouer l'utilisateur correctement
 // Creation de la chaine si elle n'existait pas deja
-func (c *Card) AddWrite(db *sql.DB, id int) error {
+func (c *Card) AddWrite(p *ConnectionPoolWrapper, id int) error {
+	db := p.GetConnection()
+	defer p.ReleaseConnection(db)
 	var wr string
 	row := db.QueryRow("select write from cards where id = $1", c.Id)
 	if err := row.Scan(&wr); err != nil {
@@ -73,7 +78,9 @@ func (c *Card) AddWrite(db *sql.DB, id int) error {
 
 // Suppression d'un utilisateur de la chaine de write
 // Ne renvoie pas d'erreur si l'utilisateur n'etait pas present
-func (c *Card) DelWrite(db *sql.DB) error {
+func (c *Card) DelWrite(p *ConnectionPoolWrapper) error {
+	db := p.GetConnection()
+	defer p.ReleaseConnection(db)
 	var wr string
 	row := db.QueryRow("select write from cards where id = $1", c.Id)
 	if err := row.Scan(&wr); err != nil {
