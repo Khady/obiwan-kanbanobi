3c089f45d9d4aa257a5d477affb67e2e9af7f721
Louis Roché <louis@louisroche.net>
2013-02-25 17:25:25 +0100

    debut de l'ajout des sessions en bdd

--- kanban/connection.go
+++ kanban/connection.go
@@ -22,30 +22,6 @@ func (c *connectionList) delConnection(conn net.Conn) {
 	}
 }
 
-// Cette fonction doit gerer l'identification d'un utilisateur (verifier qu'il n'est pas deja identifie,
-// que son nom et son mdp sont valides...)
-// Pour le moment, un message bateau est envoye pour dire que tout s'est bien passe
-// Il faudrait que cette fonction mette aussi a jour un tableau avec des duo user/connexion pour les moments ou il
-// faudra envoyer des messages a tout le monde
-func MsgIdent(conn net.Conn, msg *message.Msg) {
-	test := &message.Msg{
-		Target:    message.TARGET_IDENT.Enum(),
-		Command:   message.CMD_SUCCES.Enum(),
-		AuthorId:  proto.Uint32(1),
-		SessionId: proto.String("superchainedesession"),
-		Ident: &message.Msg_Ident{
-			Login: proto.String(*msg.Ident.Login),
-		},
-	}
-	data, err := proto.Marshal(test)
-	if err != nil {
-		fmt.Println(err)
-	}
-	LOGGER.Print("MsgIdent", len(data), data)
-	conn.Write(write_int32(int32(len(data))))
-	conn.Write(data)
-}
-
 func readMsg(conn net.Conn, msg []byte, length int) {
 	data := &message.Msg{}
 	err := proto.Unmarshal(msg[0:length], data)
