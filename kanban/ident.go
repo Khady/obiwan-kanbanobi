aa3bcdde0a8caf4a2188035d421dc424416715ac
Louis Roché <louis@louisroche.net>
2013-02-27 18:49:44 +0100

    Creation de la clef de session de l'utilisateur

--- kanban/ident.go
+++ kanban/ident.go
@@ -11,7 +11,7 @@ import (
 
 type Session struct {
 	Id          uint32
-	User_id     uint32
+	User_id     string
 	Ident_date  time.Time
 	Session_key string
 }
@@ -21,22 +21,83 @@ type Session struct {
 // Pour le moment, un message bateau est envoye pour dire que tout s'est bien passe
 // Il faudrait que cette fonction mette aussi a jour un tableau avec des duo user/connexion pour les moments ou il
 // faudra envoyer des messages a tout le monde
-func MsgIdent(conn net.Conn, msg *message.Msg) {
+func MsgIdentConnect(conn net.Conn, msg *message.Msg) {
 	sessionId := uniuri.New()
-	test := &message.Msg{
-		Target:    message.TARGET_IDENT.Enum(),
-		Command:   message.CMD_SUCCES.Enum(),
-		AuthorId:  proto.Uint32(1),
-		SessionId: proto.String(sessionId),
-		Ident: &message.Msg_Ident{
-			Login: proto.String(*msg.Ident.Login),
-		},
+	u:= User{Name: *msg.Ident.Login}
+	var err error
+	if err = u.GetByName(dbPool); err == nil {
+		session := &Session{
+			Id: 0,
+			User_id: *msg.Ident.Login,
+			Ident_date: time.Now(),
+			Session_key: sessionId,
+		}
+		err = session.Add(dbPool)
 	}
-	data, err := proto.Marshal(test)
+	var answer *message.Msg
+	if err != nil {
+		answer = &message.Msg{
+			Target:    message.TARGET_IDENT.Enum(),
+			Command:   message.CMD_ERROR.Enum(),
+			AuthorId:  proto.Uint32(0),
+			SessionId: proto.String(*msg.Ident.Login),
+			Error: &message.Msg_Error{
+				ErrorId: proto.Uint32(1), // remplacer par le vrai code d'erreur ici
+			},
+		}
+	} else {
+		answer = &message.Msg{
+			Target:    message.TARGET_IDENT.Enum(),
+			Command:   message.CMD_SUCCES.Enum(),
+			AuthorId:  proto.Uint32(u.Id),
+			SessionId: proto.String(sessionId),
+			Ident: &message.Msg_Ident{
+				Login: proto.String(*msg.Ident.Login),
+			},
+		}
+	}
+	data, err := proto.Marshal(answer)
 	if err != nil {
 		fmt.Println(err)
 	}
 	LOGGER.Print("MsgIdent", len(data), data)
 	conn.Write(write_int32(int32(len(data))))
 	conn.Write(data)
+}
+
+func MsgIdentDisconnect(conn net.Conn, msg *message.Msg) {
+	// sessionId := uniuri.New()
+	// test := &message.Msg{
+	// 	Target:    message.TARGET_IDENT.Enum(),
+	// 	Command:   message.CMD_SUCCES.Enum(),
+	// 	AuthorId:  proto.Uint32(1),
+	// 	SessionId: proto.String(sessionId),
+	// 	Ident: &message.Msg_Ident{
+	// 		Login: proto.String(*msg.Ident.Login),
+	// 	},
+	// }
+	// data, err := proto.Marshal(test)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// LOGGER.Print("MsgIdent", len(data), data)
+	// conn.Write(write_int32(int32(len(data))))
+	// conn.Write(data)
+	// session := &Session{
+	// 	Id: 0,
+	// 	User_id: *msg.Ident.Login,
+	// 	Ident_date: time.Now(),
+	// 	Session_key: sessionId,
+	// }
+}
+
+// Cette fonction a une gestion synchrone des messages (traitement les uns apres les autres, pas de traitements paralleles)
+// Il faut faire une pool de worker, un dispacher et lancer l'operation a effectuer dans le dispatch.
+func MsgIdent(conn net.Conn, msg *message.Msg) {
+	switch *msg.Command {
+	case message.CMD_CONNECT:
+		MsgIdentConnect(conn, msg)
+	case message.CMD_DISCONNECT:
+		MsgIdentDisconnect(conn, msg)
+	}
 }
