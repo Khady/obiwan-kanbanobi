361c0a395ff658e8ff3cb9e9483e87fc79cf32bf
prost p <you@example.com>
2013-03-12 15:54:30 +0100

    metadata beta

--- kanban/metadata.go
+++ kanban/metadata.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bitbucket.org/ongisnotaguild/obi-wan-kanbanobi/kanban/protocol"
+	"code.google.com/p/goprotobuf/proto"
+	"net"
+)
+
 type Metadata struct {
 	Id          int
 	Object_type int
@@ -14,9 +20,10 @@ func MsgMetadataCreate(conn net.Conn, msg *message.Msg) {
 	0,
 	0,
 	msg.Metadata.ObjectId,
+	msg.Metadata.Data,
 	msg.Metadata.DataValue }
         var answer *message.Msg
-    if err := card.Add(dbPool); err != nil {
+    if err := m.Add(dbPool); err != nil {
         
 	answer = &message.Msg{
             Target:    message.TARGET_METADATA.Enum(),
@@ -54,8 +61,74 @@ func MsgMetadataUpdate(conn net.Conn, msg *message.Msg) {
 	msg.Metadata.DataValue }
     
     var answer *message.Msg
-	if err := card.Update(dbPool); err != nil {
+	if err := m.Update(dbPool); err != nil {
 		// Envoyer un message d'erreur ici
+	answer = &message.Msg{
+	    Target:    message.TARGET_METADATA.Enum(),
+	    Command:   message.CMD_ERROR.Enum(),
+	    AuthorId:  proto.Uint32(*msg.AuthorId),
+	    SessionId: proto.String(*msg.SessionId),
+	    Error: &message.Msg_Error{
+	    ErrorId: proto.Uint32(1), // remplacer par le vrai code d'erreur ici
+	    },
+	}
+	} else {
+		// Envoyer un message de succes ici
+	answer = &message.Msg{
+	    Target:    message.TARGET_CARDS.Enum(),
+	    Command:   message.CMD_SUCCES.Enum(),
+	    AuthorId:  proto.Uint32(*msg.AuthorId),
+	    SessionId: proto.String(*msg.SessionId),
+	}
+    }
+    data, err := proto.Marshal(answer)
+    if err != nil {
+	LOGGER.Print("Impossible to marshal msg in MsgCardUpdate", err, answer)
+		return
+    }
+    conn.Write(write_int32(int32(len(data))))
+    conn.Write(data)
+}
+
+func MsgMetadataDelete(conn net.Conn, msg *message.Msg) {
+    m := &Metadata{
+	Id: *msg.Metadata.Id,
+    }
+	var answer *message.Msg
+    if err := m.Del(dbPool); err != nil {
+	// Envoyer un message d'erreur ici
+	answer = &message.Msg{
+	    Target:    message.TARGET_METADATA.Enum(),
+	    Command:   message.CMD_ERROR.Enum(),
+	    AuthorId:  proto.Uint32(*msg.AuthorId),
+	    SessionId: proto.String(*msg.SessionId),
+	Error: &message.Msg_Error{
+	    ErrorId: proto.Uint32(1), // remplacer par le vrai code d'erreur ici
+	    },
+	}
+    } else {
+	answer = &message.Msg{
+	    Target:    message.TARGET_CARDS.Enum(),
+	    Command:   message.CMD_SUCCES.Enum(),
+	    AuthorId:  proto.Uint32(*msg.AuthorId),
+			SessionId: proto.String(*msg.SessionId),
+	}
+    }
+    data, err := proto.Marshal(answer)
+    if err != nil {
+	LOGGER.Print("Impossible to marshal msg in MsgCardUpdate", err, answer)
+	return
+    }
+    conn.Write(write_int32(int32(len(data))))
+    conn.Write(data)
+}
+
+func MsgMetadatadGet(conn net.Conn, msg *message.Msg) {
+	card := &Card{
+		Id: *msg.Cards.Id,
+	}
+	var answer *message.Msg
+	if err := card.Get(dbPool); err != nil {
 		answer = &message.Msg{
 			Target:    message.TARGET_CARDS.Enum(),
 			Command:   message.CMD_ERROR.Enum(),
@@ -69,19 +142,31 @@ func MsgMetadataUpdate(conn net.Conn, msg *message.Msg) {
 		// Envoyer un message de succes ici
 		answer = &message.Msg{
 			Target:    message.TARGET_CARDS.Enum(),
-			Command:   message.CMD_SUCCES.Enum(),
+			Command:   message.CMD_GET.Enum(),
 			AuthorId:  proto.Uint32(*msg.AuthorId),
 			SessionId: proto.String(*msg.SessionId),
+			Cards: &message.Msg_Metadata{
+				Id:         proto.Uint32(card.Id),
+				ProjectId:  proto.Uint32(card.Project_id),
+				ColumnId:   proto.Uint32(card.Column_id),
+				Name:       proto.String(card.Name),
+				Desc:       proto.String(card.Content),
+				Tags:       card.Tags,
+				UserId:     proto.Uint32(card.User_id),
+				ScriptsIds: card.Scripts_id,
+				Write:      card.Write,
+			},
 		}
 	}
 	data, err := proto.Marshal(answer)
 	if err != nil {
-		LOGGER.Print("Impossible to marshal msg in MsgCardUpdate", err, answer)
+		LOGGER.Print("Impossible to marshal msg in MsgMetadataUpdate", err, answer)
 		return
 	}
 	conn.Write(write_int32(int32(len(data))))
 	conn.Write(data)
 }
+
 
 func MsgMetadata(conn net.Conn, msg *message.Msg) {
     switch *msg.Command {
